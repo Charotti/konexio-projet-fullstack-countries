cc62bc953219976bcea64b608d57c1ae5daa875f
unknown <clamy6@yahoo.fr>
2022-01-20 11:42:13 +0100

    currencies

--- front/script.js
+++ front/script.js
@@ -1,25 +1,27 @@
 function getAllcountries() {
-    $("#btnShowData").click(function(){
-        $.ajax({
-            url: 'https://restcountries.com/v3.1/all/',
-            
-            success: function(data, statuts, response) {
-                console.log(data);
-                const countriesList = data.map(function(country){
-                    console.log(country.capital);
-                    return `<li><p>${country.name.common}</p><p>${country.capital}</p></li>`
-                }) 
-                
-                // <p>${country.capital[0]}</p>
+  $("#btnShowData").click(function () {  
+    $.ajax({
+      url: "https://restcountries.com/v3.1/all/",
 
-                $("#countriesUl").html(countriesList.join(""))
-               
-     
+      success: function (data, statuts, response) {
+        console.log(data);
+       
+        const countriesList = data.map(function (country) {
+        //   console.log(country.capital);
+          let currenciesList = "";
+          if (country.currencies !== undefined) {
+            const currency = Object.keys(country.currencies);
+
+            for (i = 0; i <= currency.length; i++) {
+              currenciesList += `<p>${currency[i]}</p>`;
             }
-    });
-        
-    });
-    
+          }
+          return `<li><p>${country.name.common}</p><p>${country.capital}</p>${currenciesList}</li>`;
+        });
 
+        $("#countriesUl").html(countriesList.join(""));
+      },
+    });
+  });
 }
 getAllcountries();
